20a822eec8649e9035faa3381bd634d4237df17f
roostercrab <jesse.shea@qixingdesign.com>
2019-12-04 21:09:49 -0800

    fixed so that it works on load and stuff

--- lib/subnetter_web/controllers/page_controller.ex
+++ lib/subnetter_web/controllers/page_controller.ex
@@ -6,10 +6,16 @@ defmodule SubnetterWeb.PageController do
     render(conn, "index.html")
   end
 
-  def subnetter(conn, %{"original_ip_address" => original_ip_address, "original_subnet_mask" => original_subnet_mask} = params) do
+  def subnetter(
+        conn,
+        %{
+          "original_ip_address" => original_ip_address,
+          "original_subnet_mask" => original_subnet_mask
+        } = params
+      ) do
     ip_struct = SubnetCalc.main(original_ip_address, original_subnet_mask)
     ip_map = Map.from_struct(ip_struct)
     IO.inspect(ip_map, label: "*!*!*!*!*!ip_map inspect")
-    render(conn, "index.html", ip_map)
+    render(conn, "results.html", ip_map)
   end
 end
