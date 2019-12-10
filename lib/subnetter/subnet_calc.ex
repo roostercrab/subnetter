b72a6870933aab84848f081baf483b82e208dfa6
roostercrab <jesse.shea@qixingdesign.com>
2019-12-09 21:08:26 -0800

    ready to start combining

--- lib/subnetter/subnet_calc.ex
+++ lib/subnetter/subnet_calc.ex
@@ -365,7 +365,7 @@ defmodule SubnetCalc do
       |> String.graphemes()
       |> Enum.count(&(&1 == "1"))
     front_of_magic_octet = String.slice(magic_octet, 0, number_of_ones_in_magic_octet - 1)
-    back_of_magic_octet = String.slice(magic_octet, 0, (number_of_ones_in_mask - 8)..7)
+    back_of_magic_octet = String.slice(magic_octet, 0, (number_of_ones_in_magic_octet - 8)..7)
     {front_of_magic_octet, back_of_magic_octet}
   end
 end
