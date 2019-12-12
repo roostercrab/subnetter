22e03aa96e8abb45af9d18fda4601f5a0cb058a6
roostercrab <jesse.shea@qixingdesign.com>
2019-12-12 10:10:52 -0800

    Subnets are a-gettin figured into binary now

--- lib/subnetter/subnet_calc.ex
+++ lib/subnetter/subnet_calc.ex
@@ -1,21 +1,40 @@
 defmodule SubnetFigurin do
-  def main(original_ip_address, original_subnet_mask) do
-    original_ip_address_list = String.split(original_ip_address, ".")
-    original_subnet_mask_list = String.split(original_subnet_mask, ".")
+  def main(original_ip_address, original_mask) do
+    original_decimal_ip_address_list = String.split(original_ip_address, ".")
+    original_decimal_mask_list = String.split(original_mask, ".")
 
-    [
-      original_ip_address_first_octet,
-      original_ip_address_second_octet,
-      original_ip_address_third_octet,
-      original_ip_address_fourth_octet
-    ] = original_ip_address_list
+    original_decimal_ip_address_numbers = 
+      for octet <- original_decimal_ip_address_list do
+      String.to_integer(octet)
+    end
 
-    [
-      original_subnet_mask_first_octet,
-      original_subnet_mask_second_octet,
-      original_subnet_mask_third_octet,
-      original_subnet_mask_fourth_octet
-    ] = original_subnet_mask_list
+    original_decimal_mask_numbers = 
+      for octet <- original_decimal_mask_list do
+      String.to_integer(octet)
+    end
+
+    original_binary_ip_address_list =
+      for octet <- original_decimal_ip_address_numbers do
+        change_decimal_to_binary(octet, "", [128, 64, 32, 16, 8, 4, 2, 1])
+      end
+
+    original_binary_mask_list =
+      for octet <- original_decimal_mask_numbers do
+        change_decimal_to_binary(octet, "", [128, 64, 32, 16, 8, 4, 2, 1])
+      end
+
+    # original_binary_ip_address_list =
+    #   Enum.each(original_decimal_ip_address_numbers, fn x ->
+    #     change_decimal_to_binary(x, "", [128, 64, 32, 16, 8, 4, 2, 1])
+    #   end)
+
+    # original_binary_mask_list =
+    #   Enum.each(original_decimal_mask_numbers, fn x ->
+    #     change_decimal_to_binary(x, "", [128, 64, 32, 16, 8, 4, 2, 1])
+    #   end)
+
+    IO.inspect(original_binary_ip_address_list, label: "Original IP binary")
+    IO.inspect(original_binary_mask_list, label: "Original Subnet Mask binary")
   end
 
   def change_decimal_to_binary(value, binary, [head | tail]) do
@@ -24,28 +43,17 @@ defmodule SubnetFigurin do
     cond do
       new_value >= 0 ->
         new_binary = "#{binary}" <> "1"
-        IO.inspect(value, label: "Value")
-        IO.inspect(new_value, label: "New Value")
-        IO.inspect(new_binary, label: "New Binary")
-        IO.inspect(tail, label: "Tail")
         change_decimal_to_binary(new_value, new_binary, tail)
 
       new_value < 0 ->
         new_binary = "#{binary}" <> "0"
-        IO.inspect(value, label: "Value")
-        IO.inspect(new_value, label: "New Value")
-        IO.inspect(new_binary, label: "New Binary")
-        IO.inspect(tail, label: "Tail")
         change_decimal_to_binary(value, new_binary, tail)
     end
   end
 
   def change_decimal_to_binary(value, binary, []) do
-    IO.inspect(value, label: "VALUE")
     IO.inspect(binary, label: "BINARY")
   end
-
-
 
   def change_binary_to_decimal(binary) do
     format_binary(binary)
@@ -56,20 +64,28 @@ defmodule SubnetFigurin do
     binary_list = String.graphemes(binary)
     Enum.map(binary_list, &String.to_integer/1)
   end
-  
+
   defp combine_values([binary_head | binary_tail], [value_head | value_tail], value) do
-    new_value = (binary_head * value_head) + value
+    new_value = binary_head * value_head + value
     combine_values(binary_tail, value_tail, new_value)
   end
-    
+
   defp combine_values([], [], value) do
     IO.inspect(value, label: "VALUE")
   end
-
-
-
 end
 
+#     IO.inspect(value, label: "Value")
+#     IO.inspect(new_value, label: "New Value")
+#     IO.inspect(new_binary, label: "New Binary")
+#     IO.inspect(tail, label: "Tail")
+
+#     IO.inspect(value, label: "Value")
+#     IO.inspect(new_value, label: "New Value")
+#     IO.inspect(new_binary, label: "New Binary")
+#     IO.inspect(tail, label: "Tail")
+
+# IO.inspect(value, label: "VALUE")
 # def change_decimal_to_binary(octet) do 
 #   subtract()
 # end 
